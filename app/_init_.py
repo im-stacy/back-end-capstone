e4eab28c4067aa69af0c833552b30961ae27ded3
Siyi Ji <monkeyqueenstacy@gmail.com>
2023-02-01 09:00:53 -0700

    test commit

---
+++ app/_init_.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+                "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # db.init_app(app)
+    # migrate.init_app(app, db)
+
+    # from app.models.recipe import Recipe
+    # from app.models.ingredient import Ingredient
+
+    # db.init_app(app)
+    # migrate.init_app(app, db)
+
+    # from .routes import ingredient_bp
+    # app.register_blueprint(ingredient_bp)
+
+    # from .routes import recipe_bp
+    # app.register_blueprint(recipe_bp)
+
+    # CORS(app)
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+    return app
+
+app = create_app()
